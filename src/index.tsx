5fe6ae73e580dbf4c30f5684b72151e84005d9f7
lzajac <lksz.zajac@gmail.com>
2020-05-08 08:36:23 +0200

    add persisted token loading into axios

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers'
 import { loadState, saveState } from './util/localStorage'
+import api from './util/api'
 // import * as serviceWorker from './serviceWorker';
 
 const persistedState = loadState()
 const store = createStore(rootReducer, persistedState, (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())
-
+const auth: any = store.getState().auth
+if (auth.token !== undefined) api.defaults.headers.Token = auth.token
 store.subscribe(() => {
   saveState({
     auth: store.getState().auth
