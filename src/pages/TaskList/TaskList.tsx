12e09c4b56a7d048c44383690f09ae526b3a41fc
Łukasz Zając <lksz.zajac@gmail.com>
2020-09-03 21:09:43 +0200

    Task List

--- src/pages/TaskList/TaskList.tsx
+++ src/pages/TaskList/TaskList.tsx
@@ -1,22 +1,52 @@
 import React, { ComponentProps } from 'react'
-import { withRouter } from 'react-router'
 import '../../index.css'
-import { Button, Heading, Table, Box, Container } from 'react-bulma-components'
+import { DetailsList, IColumn, PrimaryButton, DefaultButton, Text } from 'office-ui-fabric-react'
 import { Task } from '../../model/Task'
 import api from '../../util/api'
+import { withRouter } from 'react-router'
 
 type TState = {
   [key: string]: any;
   tasks: Task[]
 };
 
+interface ITaskListItem {
+  id: string | null;
+  key: number;
+  name: string;
+  type: string;
+  time: string;
+}
+
 class TaskList extends React.Component<ComponentProps<any>> {
   readonly state: TState
+  private _allItems: ITaskListItem[]
+  private _columns: IColumn[]
 
   constructor (props: any) {
     super(props)
     this.editTask = this.editTask.bind(this)
     this.createTask = this.createTask.bind(this)
+
+    this._allItems = []
+
+    this._columns = [
+      { key: 'column1', name: '#', fieldName: 'key', minWidth: 25, maxWidth: 25, isResizable: false },
+      { key: 'column2', name: 'Tytuł', fieldName: 'name', minWidth: 100, maxWidth: 200, isResizable: true },
+      { key: 'column3', name: 'Typ', fieldName: 'type', minWidth: 100, maxWidth: 200, isResizable: true },
+      { key: 'column4', name: 'Czas', fieldName: 'time', minWidth: 100, isResizable: true },
+      {
+        key: 'column5',
+        name: '',
+        fieldName: 'time',
+        minWidth: 100,
+        isResizable: true,
+        onRender: (item: ITaskListItem) => {
+          return <DefaultButton onClick={() => this.editTask(item.id)}>Edytuj</DefaultButton>
+        },
+      },
+    ]
+
     this.state = {
       tasks: [
         {
@@ -57,12 +87,33 @@ class TaskList extends React.Component<ComponentProps<any>> {
         },
       ].map(taskResponseMock => Task.fromResponse(taskResponseMock))
     }
+
+    this._allItems = this.state.tasks.map((it, index) => (
+      {
+        key: index,
+        id: it.id,
+        name: it.name,
+        type: it.type,
+        time: it.minutes
+      }
+    )
+    )
   }
 
   componentDidMount () {
     api.get('/tasks')
       .then((response) => response.data)
       .then((tasks) => this.setState({ tasks }))
+    this._allItems = this.state.tasks.map((it, index) => (
+      {
+        key: index,
+        id: it.id,
+        name: it.name,
+        type: it.type,
+        time: it.minutes
+      }
+    )
+    )
   }
 
   editTask (id: string | null) {
@@ -76,58 +127,62 @@ class TaskList extends React.Component<ComponentProps<any>> {
   }
 
   render () {
-    const { state } = this
     return (
       <div className="page">
-        <Container>
-          <Box>
-            <Heading size={2}>Zadania</Heading>
-          </Box>
-          <Box>
-            <Table>
-              <thead>
-                <tr>
-                  <th>Tytuł</th>
-                  <th>Przedmiot</th>
-                  <th>Typ</th>
-                  <th>Czas trwania</th>
-                  <th>Działania</th>
-                </tr>
-              </thead>
-              <tfoot>
-                <tr>
-                  <th>Tytuł</th>
-                  <th>Przedmiot</th>
-                  <th>Typ</th>
-                  <th>Czas trwania</th>
-                  <th>Działania</th>
-                </tr>
-              </tfoot>
-              <tbody>
-                {state.tasks.map((task, key) => (
-                  <tr key={key}>
-                    <td>
-                      {task.id} {task.name}
-                    </td>
-                    <td>{task.subject}</td>
-                    <td>{task.type}</td>
-                    <td>{task.minutes} minut</td>
-                    <td>
-                      <Button color="info" onClick={() => this.editTask(task.id)}>
-                      Edytuj
-                      </Button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          </Box>
-          <Box>
-            <Button color="success" onClick={this.createTask}>
-              Dodaj nowe zadanie
-            </Button>
-          </Box>
-        </Container>
+        <Text variant={'xxLargePlus'}>Zadania</Text>
+        <DetailsList
+          items={this._allItems}
+          columns={this._columns}
+          // setKey="set"
+          // layoutMode={DetailsListLayoutMode.justified}
+          // selection={this._selection}
+          // selectionPreservedOnEmptyClick={true}
+          // ariaLabelForSelectionColumn="Toggle selection"
+          // ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+          // checkButtonAriaLabel="Row checkbox"
+          // onItemInvoked={this._onItemInvoked}
+        />
+        {/* <Table> */}
+        {/*  <thead> */}
+        {/*  <tr> */}
+        {/*    <th>Tytuł</th> */}
+        {/*    <th>Przedmiot</th> */}
+        {/*    <th>Typ</th> */}
+        {/*    <th>Czas trwania</th> */}
+        {/*    <th>Działania</th> */}
+        {/*  </tr> */}
+        {/*  </thead> */}
+        {/*  <tfoot> */}
+        {/*  <tr> */}
+        {/*    <th>Tytuł</th> */}
+        {/*    <th>Przedmiot</th> */}
+        {/*    <th>Typ</th> */}
+        {/*    <th>Czas trwania</th> */}
+        {/*    <th>Działania</th> */}
+        {/*  </tr> */}
+        {/*  </tfoot> */}
+        {/*  <tbody> */}
+        {/*  {state.tasks.map((task, key) => ( */}
+        {/*    <tr key={key}> */}
+        {/*      <td> */}
+        {/*        {task.id} {task.name} */}
+        {/*      </td> */}
+        {/*      <td>{task.subject}</td> */}
+        {/*      <td>{task.type}</td> */}
+        {/*      <td>{task.minutes} minut</td> */}
+        {/*      <td> */}
+        {/*        <Button color="info" onClick={() => this.editTask(task.id)}> */}
+        {/*          Edytuj */}
+        {/*        </Button> */}
+        {/*      </td> */}
+        {/*    </tr> */}
+        {/*  ))} */}
+        {/*  </tbody> */}
+        {/* </Table> */}
+
+        <PrimaryButton onClick={this.createTask}>
+          Dodaj nowe zadanie
+        </PrimaryButton>
       </div>
     )
   }
