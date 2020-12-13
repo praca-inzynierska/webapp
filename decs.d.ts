2d6c8585b2fe08ae5f1f88f839521a6b288830f3
Łukasz Zając <lksz.zajac@gmail.com>
2020-12-13 16:10:05 +0100

    ported chat logic from mobile app (unfinished)

--- decs.d.ts
+++ decs.d.ts
@@ -1,6 +1,7 @@
 declare module 'react-datepicker';
 declare module 'react-markdown';
 declare module 'react-notify-toast';
+declare module 'reactjs-simple-gifted-chat';
 
 declare interface Array<T> {
   findByKey (keyProperty: string, key: any): T;
